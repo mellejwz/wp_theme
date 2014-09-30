db7015bca1dfe1097fec96e8b9eb2879b871d446
unknown <mellezwerver@outlook.com>
2014-09-30 08:01:04 +0200

    it's workinggit add *

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/jquery-ui.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/jquery-ui.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/script.js"></script>
 	<link rel="stylesheet" href="style.css" />
 	<title><?php bloginfo('name'); ?></title>
